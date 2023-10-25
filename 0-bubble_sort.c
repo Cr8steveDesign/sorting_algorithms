49ff67fcb993bc9016051ec2817a28b6175e5969
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-10-25 13:27:38 +0100

    More sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,20 +1,36 @@
 #include "sort.h"
 
+/**
+ * bubble_sort - Implementing a bubble sort algorithm
+ * @array: Array to be sorted
+ * @size: Size of the array to be sorted
+ * Return: Returns nothing;
+ */
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t j = 0, i = 0, temp = 0;
+	size_t j = 0, i = 0, temp = 0, flag = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = i + 1; j < size; j++)
+		flag = 0;
+
+		for (j = 0; j < size; j++)
 		{
-			if (array[i] > array[j])
+			if (array[i] < array[j])
 			{
 				temp = array[i];
 				array[i] = array[j];
 				array[j] = temp;
+
+				flag = 1;
 				print_array(array, size);
 			}
+		}
+		if (flag == 0)
+		{
+			print_array(array, size);
+			break;
 		}
 	}
 }
